36396a2e448b88fd770cf9040bb47bfc2922a712
vaidehi2609 <63514929+vaidehi2609@users.noreply.github.com>
2020-04-12 00:08:47 +0530

    Create number swap.c

---
+++ number swap.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter number1:");
+scanf("%d",&a);
+printf("enter number2:");
+scanf("%d",&b);
+printf("number1=%d\n",b);
+printf("number2=%d",a);
+return(0);
+}
